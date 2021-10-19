ef8f6d7a0ee82ed88788b6706f35c3836f0cbcc6
Sergei <sergeiuniverse@gmial.com>
2021-10-19 11:28:18 +0300

    VK API added and tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const apiUrl = '/method/users.get?user_id=210700286&v=5.52';
+
+  fetch(apiUrl,{
+    method: 'GET'
+})
+    .then(res => res.json())
+    .then(
+        (result) => {
+            console.log(result);
+        },
+        (error) => {
+            console.log("Ошибка при получении данных...")
+        }
+    )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
 export default App;
+
