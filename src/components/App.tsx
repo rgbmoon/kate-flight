cf7ad7bbaf930bfe58d57f66c5b94405b34ad664
Sergei <sergeiuniverse@gmial.com>
2021-10-22 20:57:13 +0300

    added normalize css and css module

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
 
@@ -27,7 +27,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className={ styles.app }>
     </div>
   );
 }
