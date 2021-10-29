5e93443b8b8842419b0bacb1d2915f197db19408
Sergei <sergeiuniverse@gmial.com>
2021-10-29 13:24:17 +0300

    made responsive and adaptive main blocks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import logoVK from '../img/vk.png';
+import logoInsta from '../img/insta.png';
 
 function Footer() {
 
   return (
     <footer className={ styles.footer }>
-      Фуууууутер
+      <div className={ styles.socialMediaBlock }>
+        <a href="https://www.instagram.com/katee_flight/" target="_blank">
+          <img src={logoInsta} alt="Instagram" />
+        </a>
+      </div>
+      <div className={ styles.socialMediaBlock }>
+        <a href="https://vk.com/kateeeket" target="_blank">
+          <img src={logoVK} alt="VK" />
+        </a>
+      </div>
     </footer>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './Menu.module.css';
 
+const MenuItem = (props) => {
+
+  return (
+    <a className={ styles.menuItem }>
+      { props.name }
+    </a>
+  );
+}
+
 function Menu() {
 
   return (
     <div className={ styles.menu }>
-      Менюха
+      <MenuItem name="События"/>
+      <MenuItem name="Портфолио"/>
+      <MenuItem name="Стоимость"/>
+      <MenuItem name="Обо мне"/>
     </div>
   );
 }
