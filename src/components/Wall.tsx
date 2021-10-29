c0f3b0d3279ec266d2c22584469eda6fd8d0a851
Sergei <sergeiuniverse@gmial.com>
2021-10-30 02:31:35 +0300

    some adaptive improvements

--- src/components/Wall.tsx
+++ src/components/Wall.tsx
@@ -7,9 +7,6 @@ function Wall() {
     <div className={ styles.wall }>
       <div className={ styles.wallPost }>1</div>
       <div className={ styles.wallPost }>2</div>
-      <div className={ styles.wallPost }>3</div>
-      <div className={ styles.wallPost }>4</div>
-      <div className={ styles.wallPost }>5</div>
     </div>
   );
 }
