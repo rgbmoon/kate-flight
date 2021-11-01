37bd74e0aea9401d9153266d732a9c515d8d40b7
Sergei Abrashkin <sergeiuniverse@gmail.com>
2021-11-02 00:17:41 +0300

    made collapse animation from MUI kit

--- src/components/ModalText.tsx
+++ src/components/ModalText.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './ModalText.module.css'
+import Collapse from '@mui/material/Collapse'
 
 
 function ModalText() {
@@ -8,22 +9,22 @@ function ModalText() {
 
   const onClose = () => {
     setHide(!isHide)
-    console.log(isHide)
   }
 
   return (
     <div
-      className={isHide ? `${styles.modalText} ${styles.modalTextClosed}` : styles.modalText} 
+      className={isHide ? `${styles.modalText} ${styles.modalTextClosed}` : styles.modalText}
       onClick={isHide ? onClose : undefined}>
-
-      <p className={isHide ? `${styles.textHidden} ${styles.text}` : styles.text}>Привет, меня зовут Катерина. Рада видеть вас в своем уютном уголке интернета.
-        Я занимаюсь индивидуальными фотосессиями в Петербурге уже более 6 лет. Бесконечно вдохновляюсь людьми и их уникальной красотой.<br /><br />
-        Ниже можно подробно ознакомиться с моим творчеством, условиями съемок и прочей полезной информацией.
-      </p>
+      <Collapse in={!isHide} collapsedSize={16}>
+        <p className={isHide ? `${styles.textHidden} ${styles.text}` : styles.text}>Привет, меня зовут Катерина. Рада видеть вас в своем уютном уголке интернета.
+          Я занимаюсь индивидуальными фотосессиями в Петербурге уже более 6 лет. Бесконечно вдохновляюсь людьми и их уникальной красотой. <br /><br />
+          Ниже можно подробно ознакомиться с моим творчеством, условиями съемок и прочей полезной информацией.
+        </p>
+      </Collapse>
       <span
         className={isHide ? `${styles.close} ${styles.open}` : styles.close}
         onClick={onClose}></span>
-        
+
     </div>
   )
 }
