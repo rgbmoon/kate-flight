2a0a891ebfdb4da5a1b796f9492b164e7dfa58f9
Sergei Abrashkin <sergeiuniverse@gmail.com>
2021-11-06 14:38:41 +0300

    started making firebase cloud function for vk api requests

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,28 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions"
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const testRequest = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+  const token = functions.config().vk.key
+  const userId = functions.config().vk.id
+  const apiVer = functions.config().vk.apiver
+  const apiUrl = `https://api.vk.com/method/wall.get?owner_id=-${userId}&count=1&access_token=${token}&v=${apiVer}`
+
+  response.send(apiUrl)
+
+  // fetch(apiUrl, {
+  //   method: 'GET'
+  // })
+  //   .then(res => res.json())
+  //   .then(
+  //     (result) => {
+  //       functions.logger.info(result, { structuredData: true })
+  //       console.log(result)
+  //     },
+  //     (error) => {
+  //       console.log("Ошибка при получении данных...")
+  //     }
+  //   )
+})
 
