d957b9aee61278b9563a9edfd6237c763397e3a9
Sergei <sergeiuniverse@gmial.com>
2021-11-10 21:06:42 +0300

    improve fetch function for vk wall

--- src/components/Wall.tsx
+++ src/components/Wall.tsx
@@ -1,28 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Wall.module.css'
+import { Item } from '../types/types'
 
 function Wall() {
+  const [wallData, setWallData] = useState<Item[]>([])
 
-  fetch('/.netlify/functions/vk', {
-    method: 'GET'
-  })
-    .then(res => res.json())
-    .then(
-      (result) => {
-        console.log('ответ', result)
-      },
-      (error) => {
-        console.log("Ошибка при получении данных...", error)
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/.netlify/functions/vkWall')
+      
+      if (!response.ok) {
+        console.error('Ошибка получения данных', response.status)
+        return
       }
-    )
+      const data = await response.json()
+      const items = await data.response.items
+
+      setWallData(items)
+    } catch (error: any) {
+      console.error('Ошибка получения данных', error.message)
+      //TODO не забыть сделать заглущку для страницы
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  console.log('стена прилетела', wallData)
 
   return (
-    <div className={styles.wall}>
-      <div className={styles.wallPost}>1</div>
-      <div className={styles.wallPost}>2</div>
-      <div className={styles.wallPost}>1</div>
-      <div className={styles.wallPost}>2</div>
-    </div>
+    <>
+      <div className={styles.wall}>
+        {Object.keys(wallData)
+          .map(key => {
+            return (
+              <div key={key} className={styles.wallPost}>
+                {/* <img src={} /> */}
+              </div>)
+          })}
+      </div>
+    </>
   )
 }
 
