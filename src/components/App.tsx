be894df595d5a52fdc705d23b8aac266f17cdc1b
Sergei <sergeiuniverse@gmial.com>
2021-11-14 00:55:07 +0300

    made redirect from / to /home and started making final design

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,4 @@
 import styles from './App.module.css'
-import Footer from './Footer'
 import ModalText from './ModalText'
 import Tabs from './Tabs'
 import { Outlet } from "react-router-dom"
@@ -18,18 +17,17 @@ function App() {
     <div className={styles.app}>
       <div className={styles.container}>
         <div className={styles.content}>
-          <h1 className={styles.title}>Kate Flight</h1>
+          <h1 className={styles.title}>
+            kate <span className={styles.color}>flight</span>
+          </h1>
 
           <ModalText />
 
           <Tabs />
-          
+
           <Outlet />
 
         </div>
-
-        <Footer />
-
       </div>
     </div>
   )
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './Tabs.module.css'
+// import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
 import { NavLink } from "react-router-dom"
+import Wall from './Wall'
 interface TabsButtonProps {
   link: string
   title: string
@@ -12,14 +15,14 @@ const TabsButton = (props: TabsButtonProps) => {
     <NavLink
       to={props.link}
       className={({ isActive }) =>
-          [
-            styles.tabsButton,
-            isActive ? styles.active : null
-          ]
-            .filter(Boolean)
-            .join(" ")
-        }
-        >
+        [
+          styles.tabsButton,
+          isActive ? styles.active : null
+        ]
+          .filter(Boolean)
+          .join(" ")
+      }
+    >
       {props.title}
     </NavLink >
   )
@@ -29,31 +32,53 @@ const TabsButton = (props: TabsButtonProps) => {
 function Tabs() {
 
   return (
-    <React.Fragment>
-
-      <div className={styles.tabsNav}>
-        <TabsButton
-          title="События"
-          link="/home"
-        />
-        <TabsButton
-          title="Портфолио"
-          link="/portfolio"
-        />
-        <TabsButton
-          title="Стоимость"
-          link="/price"
-        />
-        <TabsButton
-          title="Контакты"
-          link="/contacts"
-        />
-      </div>
-
-      
-
-    </React.Fragment>
+    <div className={styles.tabsNav}>
+      <TabsButton
+        title="События"
+        link="/home"
+      />
+      <TabsButton
+        title="Портфолио"
+        link="/portfolio"
+      />
+      <TabsButton
+        title="Стоимость"
+        link="/price"
+      />
+      <TabsButton
+        title="Контакты"
+        link="/contacts"
+      />
+    </div>
   )
 }
+
+
+
+
+// function ScrollableTabsButtonPrevent() {
+//   const [value, setValue] = React.useState(0);
+
+//   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+//     setValue(newValue);
+//   };
+
+//   return (
+//     <div className={styles.tabsNav}>
+//       <Tabs
+//         value={value}
+//         onChange={handleChange}
+//         variant="scrollable"
+//         scrollButtons={false}
+//       >
+//         <Tab label="События" to="/home" />
+//         <Tab label="Портфолио" to="/portfolio" />
+//         <Tab label="Стоимость" to="/price" />
+//         <Tab label="Контакты" to="/contacts" />
+//       </Tabs>
+//     </div>
+//   );
+// }
+
 
 export default Tabs
