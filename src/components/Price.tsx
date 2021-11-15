f2de2e28af47c4357d77aa4d4d43c8280de9e952
Sergei <sergeiuniverse@gmial.com>
2021-11-15 14:30:34 +0300

    made final design for price component

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -4,8 +4,29 @@ import styles from './Price.module.css';
 function Price() {
 
   return (
-    <div className={ styles.price }>
-      Здесь будет статичная инфа о ценах на съемку.
+    <div className={styles.wrapper}>
+      <div className={styles.block}>
+        <div className={styles.titleWrapper}>
+          <div className={styles.title}>
+            Фотосет
+          </div>
+          <div className={styles.price}>
+            12 000 руб.
+          </div>
+        </div>
+        <p className={styles.text}>Привет, меня зовут Катерина. Рада видеть вас в своем уютном уголке интернета. Я занимаюсь индивидуальными фотосессиями в Петербурге уже более 6 лет. Бесконечно вдохновляюсь людьми и их уникальной красотой.</p>
+      </div>
+      <div className={styles.block}>
+        <div className={styles.titleWrapper}>
+          <div className={styles.title}>
+            Фотосет
+          </div>
+          <div className={styles.price}>
+            12 000 руб.
+          </div>
+        </div>
+        <p className={styles.text}>Привет, меня зовут Катерина. Рада видеть вас в своем уютном уголке интернета. Я занимаюсь индивидуальными фотосессиями в Петербурге уже более 6 лет. Бесконечно вдохновляюсь людьми и их уникальной красотой.</p>
+      </div>
     </div>
   );
 }
