f7d7ca0e5498a6cdb49adc8a48a76c9fe1122ed9
Sergei <sergeiuniverse@gmial.com>
2021-11-16 21:36:41 +0300

    made srcset for portfolio component

--- src/components/WallPost.tsx
+++ src/components/WallPost.tsx
@@ -67,28 +67,29 @@ function WallPost(props: wallPostProps) {
                   <img
                     srcSet={
                       `${photo.sizes[0].url} ${photo.sizes[0].width}w,
-                    ${photo.sizes[1].url} ${photo.sizes[1].width}w,
-                    ${photo.sizes[2].url} ${photo.sizes[2].width}w,
-                    ${photo.sizes[3].url} ${photo.sizes[3].width}w,
-                    ${photo.sizes[4].url} ${photo.sizes[4].width}w,
-                    ${photo.sizes[5].url} ${photo.sizes[5].width}w,
-                    ${photo.sizes[6].url} ${photo.sizes[6].width}w,
-                    ${photo.sizes[7].url} ${photo.sizes[7].width}w,
-                    ${photo.sizes[8].url} ${photo.sizes[8].width}w,`
+                      ${photo.sizes[1].url} ${photo.sizes[1].width}w,
+                      ${photo.sizes[2].url} ${photo.sizes[2].width}w,
+                      ${photo.sizes[3].url} ${photo.sizes[3].width}w,
+                      ${photo.sizes[4].url} ${photo.sizes[4].width}w,
+                      ${photo.sizes[5].url} ${photo.sizes[5].width}w,
+                      ${photo.sizes[6].url} ${photo.sizes[6].width}w,
+                      ${photo.sizes[7].url} ${photo.sizes[7].width}w,
+                      ${photo.sizes[8].url} ${photo.sizes[8].width}w,`
                     }
                     sizes={
-                      `(max-width: ${photo.sizes[0].width}px) ${photo.sizes[0].width},
-                  (max-width: ${photo.sizes[1].width}px) ${photo.sizes[1].width}px,
-                  (max-width: ${photo.sizes[2].width}px) ${photo.sizes[2].width}px,
-                  (max-width: ${photo.sizes[3].width}px) ${photo.sizes[3].width}px,
-                  (max-width: ${photo.sizes[4].width}px) ${photo.sizes[4].width}px,
-                  (max-width: ${photo.sizes[5].width}px) ${photo.sizes[5].width}px,
-                  (max-width: ${photo.sizes[6].width}px) ${photo.sizes[6].width}px,
-                  (max-width: ${photo.sizes[7].width}px) ${photo.sizes[7].width}px,
-                  (max-width: ${photo.sizes[8].width}px) ${photo.sizes[8].width}px,`}
+                      `(max-width: ${photo.sizes[0].width}px) ${photo.sizes[0].width}px,
+                      (max-width: ${photo.sizes[1].width}px) ${photo.sizes[1].width}px,
+                      (max-width: ${photo.sizes[2].width}px) ${photo.sizes[2].width}px,
+                      (max-width: ${photo.sizes[3].width}px) ${photo.sizes[3].width}px,
+                      (max-width: ${photo.sizes[4].width}px) ${photo.sizes[4].width}px,
+                      (max-width: ${photo.sizes[5].width}px) ${photo.sizes[5].width}px,
+                      (max-width: ${photo.sizes[6].width}px) ${photo.sizes[6].width}px,
+                      (max-width: ${photo.sizes[7].width}px) ${photo.sizes[7].width}px,
+                      (max-width: ${photo.sizes[8].width}px) ${photo.sizes[8].width}px,`
+                    }
                     src={photo.sizes[8].url}
                     className={styles.wallImg}
-                    alt=""
+                    alt="Последние события"
                     loading="lazy"
                   />
                 </div>
