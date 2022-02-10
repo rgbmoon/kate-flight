a14355b2202ce7bb4e344875a98b14f6af34638d
Sergei Abrashkin <sergeiuniverse@gmail.com>
2022-02-11 00:24:05 +0300

    refactor: styles

--- src/components/ModalText/index.tsx
+++ src/components/ModalText/index.tsx
@@ -8,45 +8,41 @@ import { Fade } from '@mui/material'
 
 /* Можно заменить sessionStorage на LocalStorage, если потребуется, 
 методы у них одинаковые */
-function getLocalStorageOrDefault(key: string, defaultValue: boolean) {
-  const stored = localStorage.getItem(key)
-  if (!stored) {
-    return defaultValue
-  }
-  return JSON.parse(stored)
-}
 
 const ModalText:FC = () => {
+  const getLocalStorageOrDefault = (key: string, defaultValue: boolean) => {
+    const stored = localStorage.getItem(key)
+    if (!stored) {
+      return defaultValue
+    }
+    return JSON.parse(stored)
+  }
 
   const [isHide, setHide] = useState(
     getLocalStorageOrDefault('hide', false)
   )
 
-  useEffect(() => {
-    localStorage.setItem('hide', JSON.stringify(isHide))
-  }, [isHide])
-
   const onClose = () => {
     setHide(!isHide)
   }
+
+  useEffect(() => {
+    localStorage.setItem('hide', JSON.stringify(isHide))
+  }, [isHide])
 
   return (
     <Fade in timeout={1000}>
       <div>
         <Collapse in={!isHide} collapsedSize={0}>
-          <div
-            className={styles.modalText}
-            onClick={onClose}>
-            <CloseIcon className={styles.close}/>
-            
-            <p className={isHide ? `${styles.textHidden} ${styles.text}` : styles.text}>Привет, меня зовут Катерина. Рада видеть вас в своей галерее.<br /><br />
-                Я занимаюсь индивидуальными фотосессиями в Петербурге уже более 6 лет. Бесконечно вдохновляюсь людьми и их уникальной красотой. <br /><br />
-                Ниже можно подробно ознакомиться с моим творчеством, условиями съемок и прочей полезной информацией.
+          <div className={styles.modalText}>
+            <p className={styles.text}>
+              Привет, меня зовут Катерина. Рада видеть вас в своей галерее.<br /><br />
+              Я занимаюсь индивидуальными фотосессиями в Петербурге уже более 6 лет. Бесконечно вдохновляюсь людьми и их уникальной красотой. <br /><br />
+              Ниже можно подробно ознакомиться с моим творчеством, условиями съемок и прочей полезной информацией.
             </p>
-            
-            <span
-              className={isHide ? `${styles.close} ${styles.open}` : styles.close}
-              onClick={onClose} />
+            <span className={styles.close} onClick={onClose} >
+              <CloseIcon className={styles.close}/>
+            </span>
           </div>
         </Collapse>
       </div>
