474cc0265a94958940d58872d1b46646664e725e
Sergei Abrashkin <sergeiuniverse@gmail.com>
2022-02-13 02:55:18 +0300

    update: Title onClick scroll to top added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,12 +5,16 @@ import { Tabs } from './components/Tabs'
 import cn from 'classnames'
 
 const Header:FC = () => {
+  const handleClick = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'})
+  }
+
   return(
     <Fade in timeout={2000}>
       <AppBar 
         position='fixed' 
         classes={{root: cn(styles.appBarRoot, )}}>
-        <h1 className={styles.title}>
+        <h1 className={styles.title} onClick={handleClick}>
                 kate <span className={styles.color}>flight</span>
         </h1>
         <Tabs />
